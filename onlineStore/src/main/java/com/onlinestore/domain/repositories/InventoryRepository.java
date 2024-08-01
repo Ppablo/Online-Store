6ecf721356e025c1fc487e3b9cba88c9a97398d7
Ppablo <giraldomoran@hotmail.com>
2024-08-01 11:15:48 -0500

    Revert "Implement Spring JPA"

--- onlineStore/src/main/java/com/onlinestore/domain/repositories/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlinestore.domain.repositories;
-
-import com.onlinestore.domain.entities.InventoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<InventoryEntity, Long> {
-}
